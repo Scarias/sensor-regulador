347978be7008a2ca456f9a043a3326b805fd1dee
Bastian Soto Iturra <bastian.sotoi@sansano.usm.cl>
2022-11-17 16:20:41 -0300

    RabbitMQ en cloud utilizada para el sender

--- matrixresolver/matrixresolver/connection/sender.py
+++ matrixresolver/matrixresolver/connection/sender.py
@@ -1,5 +1,5 @@
 from stomp import Connection
 
 
-conn = Connection([('localhost', 61614)])
+conn = Connection([('amqps://b-dfe8c9d7-2f1f-4d5f-a4ec-e50bf431bd18.mq.sa-east-1.amazonaws.com', 5671)])
 conn.connect()
