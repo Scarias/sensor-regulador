c74a56a4483957cfb676e9741abbb09d1c0509cf
Bastian Soto Iturra <bastian.sotoi@sansano.usm.cl>
2022-12-05 20:12:20 -0300

    Cliente base añadido a MatrixInfoAggregator

--- MatrixInfoAggregator/utils/filter.py
+++ MatrixInfoAggregator/utils/filter.py
@@ -1,2 +1,2 @@
-def out_range(frame) -> bool:
-    return float(frame.body.split(',')[0]) < 0 or float(frame.body.split(',')[0]) > 5
+def out_range(body) -> bool:
+    return float(body.body.split(',')[0]) < 0 or float(body.body.split(',')[0]) > 5
