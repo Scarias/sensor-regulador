cc79c470f2458060343ae76ebeafcbff205ecc4b
Bastian Soto Iturra <bastian.sotoi@sansano.usm.cl>
2022-12-08 15:09:02 -0300

    Mensaje a AWS parseado a bytes

--- MatrixInfoAggregator/utils/joiner.py
+++ MatrixInfoAggregator/utils/joiner.py
@@ -4,4 +4,4 @@ def prepare_data(data):
         if len(values) >= 1:
             pair_str = map(str, (sensor,values[0]))
             pair_joined.append(':'.join(pair_str))
-    return ';'.join(pair_joined)
+    return bytes(';'.join(pair_joined), encoding='utf-8')
