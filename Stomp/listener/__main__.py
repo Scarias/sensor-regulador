ccfbd2f9a541e16d557129ccf000debf4ac855de
Bastian Soto Iturra <bastian.sotoi@sansano.usm.cl>
2022-12-12 23:09:44 -0300

    Stomp listener configurado como receptor

--- Stomp/listener/__main__.py
+++ Stomp/listener/__main__.py
@@ -1,5 +1,18 @@
-from utils.msg_listener import ListenerLocalNoAmq
+from time import sleep
+from dotenv import dotenv_values
+
+from utils.msg_listener import get_sqs, get_message
+
+
+config = dotenv_values('.env')
+
+SLEEP_TIME = 1
+SQS_URL = config['SQS_URL']
+
 
 if __name__ == '__main__':
-    client = ListenerLocalNoAmq()
-    client.listen('sensors')
+    sqs = get_sqs()
+
+    while 1:
+        msg = get_message(sqs, SQS_URL)
+        print(f' [x] Recibido: {msg}')
--- Stomp/listener/utils/msg_listener.py
+++ Stomp/listener/utils/msg_listener.py
@@ -1,13 +1,64 @@
-from utils.base_client import AWSPikaClient
+import boto3
+import json
+from botocore.exceptions import ClientError
 
-class ListenerLocalNoAmq(AWSPikaClient):
-    def __init__(self):
-        super().__init__(aws=False)
 
-    def callback(self, ch, method, properties, body):
-        print(" [x] Received %r" % body)
-    
-    def listen(self, queue_name):
-        self.channel.queue_declare(queue=queue_name)
-        self.channel.basic_consume(queue=queue_name, on_message_callback=self.callback, auto_ack=True)
-        self.channel.start_consuming()
+def get_credentials(region):
+
+    secret_name = "lumensense/dynamodb"
+    region_name = region
+
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        raise e
+
+    secret = get_secret_value_response['SecretString']
+    creds = json.loads(secret)
+
+    return creds
+
+def get_sqs():
+
+    region_name = 'sa-east-1'
+    creds = get_credentials(region_name)
+
+    sqs = boto3.client(
+        'sqs',
+        aws_access_key_id=creds['access_id'],
+        aws_secret_access_key=creds['access_secret_key'],
+        region_name=region_name
+    )
+
+    return sqs
+
+
+def get_message(sqs, queue_url):
+
+    response = sqs.recieve_message(
+        QueueUrl=queue_url,
+        AttributeNames=[],
+        MaxNumberOfMessages=1,
+        MessageAttributeNames=['All'],
+        VisibilityTimeout=0,
+        WaitTimeSeconds=0
+    )
+
+    message = response['Messages'][0]
+    receipt_handle = message['ReceiptHandle']
+
+    sqs.delete_message(
+        QueueUrl=queue_url,
+        ReceiptHandle=receipt_handle
+    )
+
+    print(f'Message: {message}')
+    return message
